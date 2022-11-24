23e81b5373e457c999f6e1afedefb1a9a1098669
이경범 <tkdwk567@naver.com>
2022-11-24 14:27:37 +0900

    feat : 가방을 열어 티켓을 확인하고 계산하는 기능 작성

--- src/main/java/domain/Bag.java
+++ src/main/java/domain/Bag.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class Bag {
+
+    private Money money;
+    private Ticket ticket;
+    private Invitation invitation;
+    public void hold(Ticket ticket) {
+        // 초대장이 있으면 무료
+
+        // 초대장이 없으면 유료
+    }
 }
