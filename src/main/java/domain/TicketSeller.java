fa85180da22c3c66777802c5f29d50be634893a9
이경범 <tkdwk567@naver.com>
2022-11-24 14:35:57 +0900

    feat : 티켓 판매원이 관람객에게 티켓을 판매하는 기능 구현

--- src/main/java/domain/TicketSeller.java
+++ src/main/java/domain/TicketSeller.java
@@ -2,6 +2,12 @@ package domain;
 
 public class TicketSeller {
 
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
     public void sellTo(Audience audience) {
 
     }
