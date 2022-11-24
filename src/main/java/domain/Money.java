e91e3462597d9f91a5a77a2637946a58486afb90
이경범 <tkdwk567@naver.com>
2022-11-24 14:33:44 +0900

    feat : 돈 구현

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,4 +1,14 @@
 package domain;
 
 public class Money {
+
+    private Long amount;
+
+    public Money(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
 }
