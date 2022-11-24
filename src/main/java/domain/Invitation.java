e40cc412301c7ede29c3524ae2c5c9c9bd2b1eb6
이경범 <tkdwk567@naver.com>
2022-11-24 14:29:50 +0900

    feat : 초대장 구현

--- src/main/java/domain/Invitation.java
+++ src/main/java/domain/Invitation.java
@@ -1,4 +1,20 @@
 package domain;
 
+import java.time.LocalDateTime;
+
 public class Invitation {
+
+    private String to;
+    private LocalDateTime when;
+    private String message;
+    private String from;
+    private LocalDateTime createAt;
+
+    public Invitation(String to, LocalDateTime when, String message, String from) {
+        this.to = to;
+        this.when = when;
+        this.message = message;
+        this.from = from;
+        this.createAt = LocalDateTime.now();
+    }
 }
